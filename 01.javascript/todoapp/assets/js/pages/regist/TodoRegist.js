03b28b63a1d8cecf28f30d283d1162c6bb513491
rustandbone <dlehdgh14@naver.com>
2023-10-31 17:12:48 +0900

    feat: 등록 페이지 기능 구현

--- 01.javascript/todoapp/assets/js/pages/regist/TodoRegist.js
+++ 01.javascript/todoapp/assets/js/pages/regist/TodoRegist.js
@@ -3,10 +3,27 @@ import Footer from '../../layout/Footer.js';
 
 const TodoRegist = function () {
   document.querySelector('body').setAttribute('class', 'registerBody');
-  document.querySelector('body').appendChild(Footer());
+
   const page = document.createElement('section');
   page.setAttribute('id', 'page');
   page.setAttribute('class', 'registerSection');
+
+  const contentSection = document.createElement('section');
+  contentSection.setAttribute('class', 'contentSection');
+
+  const backBtnDiv = document.createElement('div');
+  const backBtn = document.createElement('button');
+  const arrowImg = document.createElement('img');
+  arrowImg.setAttribute('src', './assets/img/Arrow.svg');
+  arrowImg.setAttribute('alt', '뒤로 가기');
+  backBtn.setAttribute('class', 'backArrow');
+
+  backBtn.addEventListener('click', function () {
+    window.history.back();
+  });
+
+  backBtnDiv.appendChild(backBtn);
+  backBtn.appendChild(arrowImg);
 
   const content = document.createElement('article');
   content.setAttribute('class', 'registerArticle');
@@ -27,9 +44,20 @@ const TodoRegist = function () {
   registerBtn.setAttribute('class', 'registerBtn');
   const registerBtnText = document.createTextNode(`등록하기`);
   registerBtn.appendChild(registerBtnText);
+
+  [titleInput, contentInput].map((input) => {
+    input.addEventListener('input', function () {
+      if (titleInput.value !== '' && contentInput.value !== '')
+        registerBtn.classList.add('register-active');
+      else if (titleInput.value === '' || contentInput.value === '') {
+        registerBtn.classList.remove('register-active');
+      }
+    });
+  });
+
   registerBtn.addEventListener('click', async function () {
     if (titleInput.value === '' || contentInput.value === '') {
-      alert('제목이나 내용을 입력해주세요');
+      alert('할 일과 내용을 입력해주세요');
       return;
     }
     await axios
@@ -46,9 +74,12 @@ const TodoRegist = function () {
     location.href = 'http://localhost:3000';
   });
 
-  page.appendChild(content);
-  page.appendChild(btnDiv);
+  contentSection.appendChild(backBtnDiv);
+  contentSection.appendChild(content);
+  contentSection.appendChild(btnDiv);
   btnDiv.appendChild(registerBtn);
+  page.appendChild(contentSection);
+  page.appendChild(Footer());
 
   return page;
 };
