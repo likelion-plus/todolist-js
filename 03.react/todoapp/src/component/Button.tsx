b2def09cd162e90fe97c9aec100ebc0839e3cb65
rustandbone <dlehdgh14@naver.com>
2023-11-10 17:43:38 +0900

    react: 버튼 컴포넌트 제작
    Co-authored-by: KIMGEUNDU <KIMGEUNDU@users.noreply.github.com>

--- 03.react/todoapp/src/component/Button.tsx
+++ 03.react/todoapp/src/component/Button.tsx
@@ -1,1 +1,19 @@
-export default function Button() {}
+interface Button {
+  children: string;
+  id: string;
+  disabled?: boolean;
+  onClick(): void;
+}
+
+export default function Button({
+  children,
+  id,
+  disabled = false,
+  onClick,
+}: Button) {
+  return (
+    <button id={id} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
