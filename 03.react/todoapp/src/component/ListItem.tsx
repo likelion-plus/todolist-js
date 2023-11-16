d299f41b67c91e39456b3dfc153bbcfb5b602ac8
rustandbone <dlehdgh14@naver.com>
2023-11-16 17:05:28 +0900

    react: checkbox 분리 및 체크 메서드 추가

--- 03.react/todoapp/src/component/ListItem.tsx
+++ 03.react/todoapp/src/component/ListItem.tsx
@@ -1,34 +1,37 @@
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import defaultInstance from '@/axios';
+import { useTodoStore } from './../store/useTodoStore';
+
 export const ListItem = ({ _id, updatedAt, title, done }: TodoListMain) => {
-  const [isDone, setIsDone] = useState(false);
+  const [isDone, setIsDone] = useState(done);
+  const { edit, setEdit } = useTodoStore();
+
   const handleCheck = async () => {
     setIsDone(!isDone);
-    // API
+    setEdit(!edit);
+
     await defaultInstance.patch(`/todolist/${_id}`, {
       done: !isDone,
     });
   };
 
-  // USEEFFECT
   useEffect(() => {
     setIsDone(done);
   }, []);
+
   return (
     <>
-      <li>
-        <Link to={`info/${_id}`} className={`list-item ${done ? 'done' : ''}`}>
-          <div>
-            <input
-              className="list-item__check"
-              type="checkbox"
-              id={String(_id)}
-              checked={isDone}
-              onChange={handleCheck}
-            />
-            <label className="list-item__title">{title}</label>
-          </div>
+      <li className={`list-item ${isDone ? 'done' : ''}`}>
+        <input
+          className={`list-item__check`}
+          type="checkbox"
+          id={String(_id)}
+          checked={isDone}
+          onChange={handleCheck}
+        />
+        <Link to={`info/${_id}`}>
+          <label className="list-item__title">{title}</label>
           <p className="list-item__date">{updatedAt.slice(0, 11)}</p>
         </Link>
       </li>
