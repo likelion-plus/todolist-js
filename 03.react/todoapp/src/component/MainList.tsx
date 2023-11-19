717b8c30deb6fd3f77e07a4dbe9d6a8cb66d3c5f
rustandbone <dlehdgh14@naver.com>
2023-11-20 00:10:24 +0900

    feat: 정렬, 검색 기능 구현

--- 03.react/todoapp/src/component/MainList.tsx
+++ 03.react/todoapp/src/component/MainList.tsx
@@ -1,36 +1,81 @@
 import { ListItem } from '@/component';
+import { useState } from 'react';
+import { useTodoStore } from './../store/useTodoStore';
 
-export const MainList = ({ selected, list }: { selected: string }) => {
+export const MainList = ({ selected, list, setList }: { selected: string }) => {
   const todo = selected === '📝 Todo';
   const done = selected === '✅ Done';
 
+  const [newSort, setNewSort] = useState(true);
+  const [userInput, setUserInput] = useState('');
+  const [filterList, setFilterList] = useState([]);
+  const { edit, setEdit } = useTodoStore();
+
+  function handleSorting() {
+    if (newSort) {
+      setList(
+        list.toSorted(
+          (a: TodoListMain, b: TodoListMain) =>
+            +new Date(a.updatedAt) - +new Date(b.updatedAt)
+        )
+      );
+      setNewSort(false);
+    } else {
+      setList(
+        list.toSorted(
+          (a: TodoListMain, b: TodoListMain) =>
+            +new Date(b.updatedAt) - +new Date(a.updatedAt)
+        )
+      );
+      setNewSort(true);
+    }
+  }
+
+  const getValue = (e) => {
+    setUserInput(e.target.value);
+    const trim = e.target.value.trim();
+    if (userInput !== '') {
+      const searched = list.filter((item) => {
+        return item.title.includes(trim);
+      });
+      setFilterList(searched);
+    }
+  };
+
   return (
     <>
+      <button className="sortBtn" type="button" onClick={() => handleSorting()}>
+        {newSort ? '등록 순으로 보기' : '최신 순으로 보기'}
+      </button>
+      <input type="text" onChange={getValue} />
       <ul className="list-container">
-        {list &&
+        {userInput.length > 1 &&
+          filterList.map((item: TodoListMain) => (
+            <ListItem
+              key={item._id}
+              _id={item._id}
+              title={item.title}
+              updatedAt={item.updatedAt}
+              done={item.done}
+            />
+          ))}
+        {userInput.length <= 1 &&
+          list &&
           !todo &&
           !done &&
-          list
-            .sort(
-              (a: TodoListMain, b: TodoListMain) =>
-                +new Date(b.updatedAt) - +new Date(a.updatedAt)
-            )
-            .map((item: TodoListMain) => (
-              <ListItem
-                key={item._id}
-                _id={item._id}
-                title={item.title}
-                updatedAt={item.updatedAt}
-                done={item.done}
-              />
-            ))}
-        {list &&
+          list.map((item: TodoListMain) => (
+            <ListItem
+              key={item._id}
+              _id={item._id}
+              title={item.title}
+              updatedAt={item.updatedAt}
+              done={item.done}
+            />
+          ))}
+        {userInput.length <= 1 &&
+          list &&
           todo &&
           list
-            .sort(
-              (a: TodoListMain, b: TodoListMain) =>
-                +new Date(b.updatedAt) - +new Date(a.updatedAt)
-            )
             .filter((item: TodoListMain) => !item.done)
             .map((item: TodoListMain) => (
               <ListItem
@@ -41,13 +86,10 @@ export const MainList = ({ selected, list }: { selected: string }) => {
                 done={item.done}
               />
             ))}
-        {list &&
+        {userInput.length === 1 &&
+          list &&
           done &&
           list
-            .sort(
-              (a: TodoListMain, b: TodoListMain) =>
-                +new Date(b.updatedAt) - +new Date(a.updatedAt)
-            )
             .filter((item: TodoListMain) => item.done)
             .map((item: TodoListMain) => (
               <ListItem
